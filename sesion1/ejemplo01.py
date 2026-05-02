96d8b6ddd65431caf59686873af9725ef01ce3d8
nestormujica33 <nestormujica33@gmail.com>
2026-05-02 09:03:37 -0400

    inicio

---
+++ sesion1/ejemplo01.py
@@ -0,0 +1,14 @@
+#Ejemplo de piton para formacion complementaria
+x = 5
+y = 10
+
+print ("el valor de 5 + 10 es: ", x * y)
+print ("Realizado")
+
+nombre = input("coloca tu primer nombre")
+apellido = input("coloca el apellido")
+
+print("el nombre completo es: " + nombre + "" + apellido)
+edad = int(input("ingrese su edad"))
+
+
