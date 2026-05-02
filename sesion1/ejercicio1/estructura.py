ad7dbfb9e3a25bb1f3cf3a1c1befab3dfb8b5271
nestormujica33 <nestormujica33@gmail.com>
2026-05-02 10:28:35 -0400

    estructuras

---
+++ sesion1/ejercicio1/estructura.py
@@ -0,0 +1,17 @@
+x = 3
+if x > 0:
+    print("positivo")
+elif x == 0:
+    print("cero")    
+else:
+    print("negativo")    
+
+for i in range(3):
+    print(f"i={i}") 
+
+
+
+n=3
+while n > 0:
+    print(n)
+    n -= 1
